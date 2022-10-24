3efa2e0b5d9410248bd80a37a7d97fdd733e3ee6
vanGalilea <galiliziv@gmail.com>
2022-10-24 16:28:49 +0200

    fix: fixes white screen of death on start

--- projects/WhatsAppClone/ios/WhatsAppCloneReactNative/AppDelegate.mm
+++ projects/WhatsAppClone/ios/WhatsAppCloneReactNative/AppDelegate.mm
@@ -44,7 +44,7 @@ static NSString *const kRNConcurrentRoot = @"concurrentRoot";
 #endif
 
   NSDictionary *initProps = [self prepareInitialProps];
-  UIView *rootView = RCTAppSetupDefaultRootView(bridge, @"WhatsAppClone", initProps);
+  UIView *rootView = RCTAppSetupDefaultRootView(bridge, @"WhatsAppCloneReactNative", initProps);
 
   if (@available(iOS 13.0, *)) {
     rootView.backgroundColor = [UIColor systemBackgroundColor];
